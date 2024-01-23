92144b10243fb8d0c2a83d66792f82a2253c7208
bang-dev <bangvo@vn.5sense.co.jp>
2024-01-23 15:34:53 +0700

    Update api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthenticationController;
 use App\Http\Controllers\Api\BlgUserController;
+use App\Http\Controllers\Api\BlgAuthorController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -33,9 +34,20 @@ Route::group([
     'controller' => BlgUserController::class,
     'middleware' => ['auth:api', 'checkRoleAdmin']
 ], function ($router) {
-    Route::post('/register', [BlgUserController::class, 'authRegister']);
+    Route::post('/register', [BlgUserController::class, 'authRegister'])->middleware(['auth:api']);
     Route::get('/', [BlgUserController::class, 'index']);
     Route::get('/{id}', [BlgUserController::class, 'show']);
-    Route::put('/{id}', [BlgUserController::class, 'update']);
+    Route::put('/update/{id}', [BlgUserController::class, 'updateUser'])->middleware(['auth:api']);
     Route::delete('/{id}', [BlgUserController::class, 'destroy']);
+    Route::post('/reset-password/{email}', [BlgUserController::class, 'resetPassword'])->middleware(['auth:api']);
+});
+
+Route::group([
+    'prefix' => 'authors',
+    'controller' => BlgAuthorController::class,
+    'middleware' => ['auth:api', 'checkRoleAdmin']
+], function ($router) {
+    Route::post('/create', [BlgAuthorController::class, 'createAuthor'])->middleware(['auth:api']);
+    Route::get('/', [BlgAuthorController::class, 'getAllAuthors'])->middleware(['auth:api']);
+    Route::put('/update/{id}', [BlgAuthorController::class, 'updateAuthor'])->middleware(['auth:api']);
 });
