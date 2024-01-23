080de738b21502ccd36c92e626e4d4099cb6923e
bang-dev <bangvo@vn.5sense.co.jp>
2024-01-23 15:27:51 +0700

    Update: JwtService

--- src/app/Services/JwtService.php
+++ src/app/Services/JwtService.php
@@ -1,15 +1,20 @@
 <?php
 namespace App\Services;
 
+use Illuminate\Support\Str;
+use App\Models\BlgUser;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Resources\Authentication\JwtResource;
 
 class JwtService{
 
     // Generate a dynamic value
-    private function generateDynamicValue(){
-        return uniqid(mt_rand(), true);
+    private function generateDynamicValue()
+    {
+        $uniqid = uniqid(mt_rand(), false);
+        return substr($uniqid, 0, 16);
     }
+
 
     public function generateHashedPassword($userInputPassword) {
         
@@ -23,8 +28,7 @@ class JwtService{
     public function createNewAccessToken($token)
     {
         $jwt = JwtResource::make($token);
-        // dd( $jwt);
-        return $jwt;
+        return  $jwt;
     }
 }
 
